cab6082f123f0d1eb0e215445bfbb3d90a9e012d
Magdalena Strójwąs <you@example.com>
2018-09-18 21:15:37 +0200

    first files & components

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import {EnterPlan} from './EnterPlan';
+import {Calendar} from "./calendar";
+
+class App extends Component {
+
+
+    render() {
+        return (
+            <div>
+                <EnterPlan/>
+                <Calendar/>
+            </div>
+        )
+    }
+}
+
+
+export default App;
---
+++ src/EnterPlan.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const EnterPlan = () => (
+    <div>
+        <h1>Enter your holiday plan</h1>
+        <span>podaj datę</span>
+    </div>
+);
